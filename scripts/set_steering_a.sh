310bd2bcb0065434a88244fe228c1b059fb447ab
Sergio A. Mart <sergio@earthrover.cc>
2018-03-15 14:01:51 +0000

    adding scripts and launcher to get 4ws working with gazebo

---
+++ scripts/set_steering_a.sh
@@ -0,0 +1,2 @@
+rostopic pub -1 /four_wheel_steering_controller/cmd_four_wheel_steering four_wheel_steering_msgs/FourWheelSteering '{front_steering_angle: 1.0, rear_steering_angle: 1.0, front_steering_angle_velocity: 0.5, rear_steering_angle_velocity: 0.5, speed: 0.25, acceleration: 0.5, jerk: 0.5}'
+
