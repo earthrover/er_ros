47703f5e8d588ac4c856b8699bdf570ced07f881
Sergio A. Mart <sergio@earthrover.cc>
2018-03-21 14:50:33 +0000

    [Platform] Changed the platform into the Resin.io
    We replace the PI with a resin.io new board that uses the compute module
    
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_all.sh
+++ scripts/launch_all.sh
@@ -2,13 +2,21 @@
 
 d=$(date)
 echo "LAUNCH ROS $d"
+/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh LAUNCH_EARTH_ROVER
+sleep 60
 
 echo "Boot $d" >> /home/earth/ros_start.log
 . /home/earth/.bashrc
 roscore &
-sleep 15
+sleep 10
+
+/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh ROS_LAUNCHED 
+
 /home/earth/catkin_ws/src/earth-rover-ros/scripts/earth_launch.sh &
-sleep 30
+sleep 20 
+
+/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh JOY_CONTROL
+
 /home/earth/catkin_ws/src/earth-rover-ros/scripts/launch_4ws_joy.sh 
 d=$(date)
 
