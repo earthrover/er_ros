18dc089a31c98609510e2dc781fd261aef69f04c
Sergio A. Mart <sergio@earthrover.cc>
2018-03-26 17:13:05 +0100

    [Launch] Verbose launch sequence
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_all.sh
+++ scripts/launch_all.sh
@@ -3,19 +3,33 @@
 d=$(date)
 echo "LAUNCH ROS $d"
 /home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh LAUNCH_EARTH_ROVER
-sleep 60
+
+for i in `seq 1 10`;
+do
+    /home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh SYSTEM_LAUNCH_$i
+    sleep 1
+done    
 
 echo "Boot $d" >> /home/earth/ros_start.log
 . /home/earth/.bashrc
 roscore &
 sleep 10
 
-/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh ROS_LAUNCHED 
+for i in `seq 1 10`;
+do
+    /home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh ROS_LAUNCHED_$i
+    sleep 1
+done
 
 /home/earth/catkin_ws/src/earth-rover-ros/scripts/earth_launch.sh &
-sleep 20 
 
-/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh JOY_CONTROL
+for i in `seq 1 10`;
+do
+    /home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh CONTROL_$i
+    sleep 1
+done
+
+/home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh 
 
 /home/earth/catkin_ws/src/earth-rover-ros/scripts/launch_4ws_joy.sh 
 d=$(date)
