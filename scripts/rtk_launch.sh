4d4808c730f9d546bd4f6863846c7a092a80a81b
Paul Harter <paul@glowinthedark.co.uk>
2018-04-04 17:09:51 +0100

    new config for rtk

--- scripts/rtk_launch.sh
+++ scripts/rtk_launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-roslaunch ublox_gps ublox_device.launch node_name:=earth_gps param_file_name:=c94_m8p_rover
+roslaunch ublox_gps ublox_device.launch node_name:=earth_gps param_file_name:=earth_rover
 
 $SHELL
