8164059d1c33512688980c18aaa7ccc058e77eef
Sergio A. Mart <sergio@earthrover.cc>
2018-04-06 17:02:21 +0100

    [Launch] Fix to startup sequence order
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_all.sh
+++ scripts/launch_all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sleep 5
 d=$(date)
 echo "LAUNCH ROS $d"
 /home/earth/catkin_ws/src/earth-rover-ros/displays/display_text.sh LAUNCH_EARTH_ROVER
--- scripts/rtk_launch.sh
+++ scripts/rtk_launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+sleep 25
 roslaunch ublox_gps ublox_device.launch node_name:=earth_gps param_file_name:=earth_rover
 
 $SHELL
