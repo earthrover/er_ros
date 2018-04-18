daace669a2faea0bf737f20f64b44abfdac0c379
paulharter <sergio@earthrover.cc>
2018-04-18 19:27:00 +0000

    adding waypoints to joy pad controls

--- earth_steering/scripts/transform.py
+++ earth_steering/scripts/transform.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
+import os
+from threading import Lock
 import rospy
+from sensor_msgs.msg import NavSatFix
+
+
 from sensor_msgs.msg import Joy
 from four_wheel_steering_msgs.msg import FourWheelSteering
 
+import navigation_api
+
+mutex = Lock()
+
+node = None
+
+def fix_callback(data):
+    mutex.acquire()
+    try:
+        node.state["fix"] = {
+            "status": data.status.status,
+            "service": data.status.service,
+            "lat": data.latitude,
+            "lon": data.longitude,
+            "alt": data.altitude,
+        }
+    finally:
+        mutex.release()
 
 
-
-class SteeringTransform(object):
+class SteeringTransformNode(object):
 
 
     def __init__(self):
@@ -15,6 +37,9 @@ class SteeringTransform(object):
         self.next_sec = None
         self.next_nano = None
         self.every_nanos = 100000000
+        self.button_states = 19 * [0]
+        self.button_handlers = 19 * [None]
+        self.state = {}
         
         
         self.velocity_scale = 0.5
@@ -26,16 +51,89 @@ class SteeringTransform(object):
         self.acceleration = 0.5
         self.jerk = 0.5
         
+        self.button_handlers[3] = self.nav_start
+        self.button_handlers[13] = self.nav_clear
+        self.button_handlers[14] = self.nav_reset
+        self.button_handlers[15] = self.nav_pause
+        self.button_handlers[12] = self.nav_add
+        
+        dir = os.path.dirname(__file__)
+        
+        self.nav_file_path = os.path.join(dir, "geo_wp.txt")
+        
         topic = "/four_wheel_steering_controller/cmd_four_wheel_steering"
         message_type = FourWheelSteering
         
         self.publisher = rospy.Publisher(topic, message_type, queue_size=10)
+        
+        navigation_api.init_api()
+
         
     def time_delta(self, sec, nano):
         sec_delta = (sec - self.last_sec) * 1000000000
         nano_delta = nano - self.last_nano
         return sec_delta + nano_delta
         
+        
+    def nav_start(self):
+        # self.send_waypoints()
+        navigation_api.start()
+    
+    def nav_pause(self):
+        navigation_api.pause()
+        
+    def nav_clear(self):
+        navigation_api.pause()
+        navigation_api.cancel()
+    	
+    
+    def nav_reset(self):
+        navigation_api.pause()
+    	navigation_api.cancel()
+        if os.path.exists(self.nav_file_path):
+            os.remove(self.nav_file_path)
+        with open(self.nav_file_path, 'w') as f:
+        	f.write("Type: geo\n")
+        	
+        	
+    def send_waypoints(self):
+    
+        if not os.path.exists(self.nav_file_path):
+            return
+            
+        cartesian = False
+            
+        with open(self.nav_file_path, 'r') as f:
+            for line in f.readlines():
+        	    if "Type" in line:
+        	        if "cartesian" in line:
+        	            cartesian = True
+        	    else:
+        	        x, y, a = line.split(" ")
+        	        if cartesian:
+        	        	navigation_api.add_waypoint_cartesian(float(x), float(y), float(a))
+        	        else:
+        	            navigation_api.add_waypoint(float(x), float(y), float(a))
+        	        
+        
+        
+    def nav_add(self):
+    
+        if not os.path.exists(self.nav_file_path):
+        	with open(self.nav_file_path, 'a') as f:
+        	    f.write("Type: geo\n") 
+        	    
+        mutex.acquire()
+        fix = self.state["fix"].copy()
+        mutex.release()
+    
+        with open(self.nav_file_path, 'a') as f:    
+            x = fix["lat"]
+            y = fix["lon"]
+            bearing = 0
+            f.write("%s %s %s\n" % (x, y, bearing))
+        
+  
         
     def get_velocity(self, joy):
     
@@ -93,9 +191,23 @@ class SteeringTransform(object):
     	    jerk=self.jerk)
     
         self.publisher.publish(msg)
+        
+        
+    def check_buttons(self, joy):
+        for i, button_state in enumerate(joy.buttons):
+            
+            if button_state == 1 and self.button_states[i] == 0:
+        		handler = self.button_handlers[i]
+        		print("pressed button %s" % i)
+        		if handler is not None:
+        		    handler()
+            self.button_states[i] = button_state
+        
     
   
     def __call__(self, joy):
+    
+        self.check_buttons(joy)
 
         velocity = self.get_velocity(joy)
         front, back = self.get_angles(joy)
@@ -104,11 +216,12 @@ class SteeringTransform(object):
 
     
 def listener():
-
-    transform = SteeringTransform()
+    global node
+    node = SteeringTransformNode()
 
     rospy.init_node('earth_steering', anonymous=True)
-    rospy.Subscriber("joy", Joy, transform)
+    rospy.Subscriber("joy", Joy, node)
+    rospy.Subscriber("/gps/fix", NavSatFix, fix_callback)
     rospy.spin()
 
 if __name__ == '__main__':
