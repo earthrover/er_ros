0388811b180abc9449cf46cae7fa5da0e77a16f7
Sergio A. Mart <sergio@earthrover.cc>
2018-05-04 13:26:26 +0100

    [Battery] Just launch the battery once
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_battery.sh
+++ scripts/launch_battery.sh
@@ -6,14 +6,14 @@ echo "BATTERY" > /home/earth/display.txt
 
 sleep $1
 
-while true; do
+#while true; do
         echo "BATTERY" > /home/earth/display.txt
         #roslaunch earth_rover earth_rover_robot_launch.launch
 
 	screen /dev/serial/by-id/usb-Arduino__www.arduino.cc__Arduino_Mega_2560_740343130393511152C0-if00 57600
         echo "BATTERY RETRY" > /home/earth/display.txt
         sleep 10
-done
+#done
 
 $SHELL
 
