e873058c2a2e2dec4b04cce21b99452c3a05e97c
Sergio A. Mart <sergio@earthrover.cc>
2018-05-04 16:39:12 +0100

    [Launch] Reorder a bit the launch system
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_imu.sh
+++ scripts/launch_imu.sh
@@ -3,7 +3,7 @@ echo "IMU $1 SECS WAIT"
 sleep $1
 while true; do
         echo "IMU PUBLISHER" > /home/earth/display.txt
-	roslaunch bosch_imu_driver imu.launch
+        roslaunch bosch_imu_driver imu.launch
         echo "IMU RETRY" > /home/earth/display.txt
         sleep 10
 done
