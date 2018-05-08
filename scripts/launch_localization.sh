ecc60fe54f901295e4d80999d71f4e79c02d69b3
Sergio A. Mart <sergio@earthrover.cc>
2018-05-08 13:49:28 +0100

    [Launch] Fixed launch sequence for localization
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/launch_localization.sh
+++ scripts/launch_localization.sh
@@ -3,7 +3,7 @@ echo "Localization $1 SECS WAIT"
 sleep $1
 while true; do
         echo "Localization" > /home/earth/display.txt
-        roslaunch earth_rover_description earth_rover_localization.launch
+        roslaunch earth_rover earth_rover_localization.launch
         echo "Localization RETRY" > /home/earth/display.txt
         sleep 10
 done
