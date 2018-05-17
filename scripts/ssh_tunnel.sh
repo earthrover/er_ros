8768a21048a4c70acdcdb1a81245aa36502e485b
Sergio A. Mart <sergio@earthrover.cc>
2018-05-17 10:56:49 +0100

    [Navigation] Work on getting navigation working
    Signed-off-by: Sergio A. Mart <sergio@earthrover.cc>

--- scripts/ssh_tunnel.sh
+++ scripts/ssh_tunnel.sh
@@ -6,10 +6,10 @@ echo "SSH $d WAIT $1"
 sleep $1
 
 	echo "CONNECTING SSH"	
-	autossh -M 0 -fTNv -R 11311:localhost:11311 earth_rover@engineer.blue &
+	autossh -M 0 -fTNv -R 11311:localhost:11311 earth_rover@engineer.blue 
 	sleep 1
 	echo "SSH 2"
-	autossh -M 0 -fTNv -R 11380:localhost:80 earth_rover@engineer.blue &
+	autossh -M 0 -fTNv -R 11380:localhost:80 earth_rover@engineer.blue
 	sleep 1
 	echo "SSH 3"
 	autossh -M 0 -fTNv -R 11322:localhost:22 earth_rover@engineer.blue 
