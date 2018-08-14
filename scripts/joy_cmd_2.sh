afa37538586ff7eba21d62209a8218a9e555791d
Sergio A. Martinez <sergio@engineer.blue>
2018-08-14 11:41:09 +0000

    [Camera] Added inputs for camera to display information with joystick
    Signed-off-by: Sergio A. Martinez <sergio@engineer.blue>

--- scripts/joy_cmd_2.sh
+++ scripts/joy_cmd_2.sh
@@ -1,2 +1,30 @@
-echo "test 2" > /home/earth/display.txt
+#!/bin/bash
 
+IR_FILE=~/ir.txt
+DEPTH_FILE=~/depth.txt
+COLOR_FILE=~/color.txt
+
+if [ -f $IR_FILE ]; then
+    rm $IR_FILE
+    touch $DEPTH_FILE
+    echo "SHOW DEPTH" 
+    exit 1
+fi
+
+
+if [ -f $DEPTH_FILE ]; then
+    rm $DEPTH_FILE
+    touch $COLOR_FILE
+    echo "SHOW COLOR"
+    exit 1
+fi
+
+
+if [ -f $COLOR_FILE ]; then
+    rm $COLOR_FILE
+    touch $IR_FILE
+    echo "SHOW IR"
+    exit 1
+fi
+
+touch $DEPTH_FILE
